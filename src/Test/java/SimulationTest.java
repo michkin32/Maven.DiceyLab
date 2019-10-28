efd73119d4ac9a5d5c1c591b219e4ba0f5df4505
Mike King <michkin32@gmail.com>
2019-10-27 20:06:44 -0400

    Worked on simulation

--- src/Test/java/SimulationTest.java
+++ src/Test/java/SimulationTest.java
@@ -6,5 +6,9 @@ public class SimulationTest {
 
     @Test
     public void runSimulation() {
+        Simulation sim1 = new Simulation(2,10);
+        Integer expected = sim1.runSimulation();
+
+        assertNotEquals(expected, sim1.tallyBin.bins[0]);
     }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -8,11 +8,13 @@ public Simulation(Integer numberOfDies, Integer numberOfTosses) {
     this.numberOfDies = numberOfDies;
     this.numberOfTosses = numberOfTosses;
 }
-public void runSimulation() {
+public Integer runSimulation() {
 
     Dice dice = new Dice();
     for(int i = 0; i < numberOfTosses; i++){
-        tallyBin.incrementBin(dice.rollDice(numberOfDies);
+        tallyBin.incrementBin(dice.rollDice(numberOfDies));
+
     }
+    return tallyBin.returnBinTally(2);
 }
 }
