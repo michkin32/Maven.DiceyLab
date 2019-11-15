f09945404cbf29edda7bbe544851b427fd8cfee5
Mike King <michkin32@gmail.com>
2019-11-15 11:38:20 -0500

    finished

--- src/Test/java/SimulationTest.java
+++ src/Test/java/SimulationTest.java
@@ -6,9 +6,45 @@ public class SimulationTest {
 
     @Test
     public void runSimulation() {
-        Simulation sim1 = new Simulation(2,10);
-        Integer expected = sim1.runSimulation();
+        Simulation sim1 = new Simulation(2, 100);
 
-        assertNotEquals(expected, sim1.tallyBin.bins[0]);
+        Bins actual;
+
+
+        /*for (int i = 0; i < 100; i++) {
+            actual = sim1.runSimulation();
+
+            Integer minExpected = 2;
+            assertTrue(minExpected <= actual);
+
+
+            actual = sim1.runSimulation();
+            Integer maxExpected = 0;
+            assertTrue(maxExpected >= actual);
+        }*/
+
+    }
+    @Test
+    public void runSimulationTest2() {
+        Simulation sim1 = new Simulation(2, 100);
+
+
+
+    }
+
+    @Test
+    public void testToString() {
+        Simulation sim1 = new Simulation(2, 1000000);
+        Bins results = sim1.runSimulation();
+        System.out.print(sim1.toString(results));
+
+    }
+
+    @Test
+    public void totalRolls() {
+    }
+
+    @Test
+    public void numberOfstars() {
     }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -8,13 +8,51 @@ public Simulation(Integer numberOfDies, Integer numberOfTosses) {
     this.numberOfDies = numberOfDies;
     this.numberOfTosses = numberOfTosses;
 }
-public Integer runSimulation() {
+public Bins runSimulation() {
 
     Dice dice = new Dice();
     for(int i = 0; i < numberOfTosses; i++){
         tallyBin.incrementBin(dice.rollDice(numberOfDies));
 
     }
-    return tallyBin.returnBinTally(2);
+    return tallyBin;
 }
+
+public String toString(Bins bin){
+    Double total = totalRolls(bin);
+    String finalResult = "***\n" +
+            "Simulation of " + numberOfDies + " dice tossed for " + numberOfTosses + " times.\n" +
+            "***"+"\n"
+            +"\n";
+    for(int i = 2; i < bin.bins.length; i++){
+            Integer dieroll = bin.bins[i];
+            Double average = bin.bins[i]/total;
+            if (i <= 9 && dieroll < 100000){
+            finalResult +=" " + " " + i + ":   " + dieroll + ": " + dieroll/total + numberOfstars(average,total) + "\n";
+            }else if (i <= 9 && dieroll > 100000){
+                finalResult += " " +  " " + i + ":  " + dieroll + ": " + dieroll/total + numberOfstars(average,total) + "\n";
+            }else if(i >= 9 && dieroll < 100000){
+                finalResult += " " + i + ":   " + dieroll + ": " + dieroll/total + numberOfstars(average,total) + "\n";
+            }
+    }
+
+    return finalResult;
+}
+public Double totalRolls(Bins bin){
+    Double sum = 0.0;
+    for(int i = 2; i < bin.bins.length; i++){
+        sum += bin.bins[i];
+    }
+    return sum;
+}
+public String numberOfstars(Double average, Double total){
+    StringBuilder sb = new StringBuilder();
+    Integer n = (int) (average * 100);
+
+    for (int i = 0; i < n; i++){
+        sb.append("*");
+    }
+    return sb.toString();
+}
+
 }
